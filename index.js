897e27b194ea75f1798af8d6371d2b0d8bb491e3
odaya frauendorff navarro <odayafrauendorffnavarro@MacBook-Pro-de-odaya.local>
2025-02-26 13:20:25 +0100

    backend phonebook 3.9 'Conectar el backend con el frontend. Cambio en las URL. Imprimir en consola datos de la solicitud o resques.body de las solicitudes POST'

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -28,6 +27,22 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+
+app.use(requestLogger)
+
+const cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
