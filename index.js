9cb7fd2829b6b72d4334ba312f8e5c0a8bc00bf7
odaya frauendorff navarro <odayafrauendorffnavarro@MacBook-Pro-de-odaya.local>
2025-03-07 12:32:02 +0100

    backend phonebook 3.15 'Añadir petición DELETE y que se refleje en la base de datos'

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const password = process.argv[2]
 const url = `mongodb+srv://fullstackopen:${password}@cluster0.vvyub.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
-mongoose.connect(url)
 
 app.use(express.static('dist'))
 
@@ -52,22 +51,24 @@ app.get('/info', (request, response) => {
   `);
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)   
-  
-  if (person) {
-      response.json(person)
-  } else {
-      response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
